c7e38fc414b392059ce48e636d02b7958880a072
rowenarono95 <rowenarono@gmail.com>
2020-10-24 21:58:51 +0300

    initiate login

---
+++ manage.py
@@ -0,0 +1,13 @@
+from app import create_app,db
+from app.models import User
+
+from  flask_migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
