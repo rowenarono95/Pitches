b161dd774a88932ec7858b7a00520aab69fdf51a
rowenarono95 <rowenarono@gmail.com>
2020-10-26 18:08:39 +0300

    add new pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField,SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Required
 import email_validator
 
 class PitchesForm(FlaskForm):
 
     text = TextAreaField('Text',validators=[Required()])
-    category = SelectField('Type',choices=[('interview','Interview pitch'),('product','Product pitch'),('dvertisement','Advertisement')],validators=[Required()])
+    category = SelectField('Type',choices=[('interview','Interview pitch'),('product','Product pitch'),('advertisement','Advertisement')],validators=[Required()])
     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
-from .forms import LoginForm,RegistrationForm
+from .forms import PitchesForm
 from flask_login import login_required,current_user
+from ..models import Pitches, User
 
 
 
@@ -13,4 +14,24 @@ def index():
 @main.route('/profile')
 def profile():
     pass
+
+@main.route('/pitch/new_pitches',methods=['POST','GET'])
+@login_required
+def new_pitch():
+    pitch_form = PitchesForm()
+    if pitch_form.validate_on_submit():
+        pitch = pitch_form.text.data
+        category = pitch_form.category.data
+
+
+     # Updated pitch instance
+        new_pitch = Pitches(pitch_comment=pitch,pitch_category=category,user=current_user)
+
+        # Save pitch method
+        new_pitch.save_pitch()
+        return redirect(url_for('.index'))
+
+    title = 'New pitch'
+    return render_template('new_pitch.html',pitchform=pitch_form )
+
 
