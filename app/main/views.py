cf05d254c346cd1cccf9cc863a99c599b022d9f8
rowenarono95 <rowenarono@gmail.com>
2020-10-26 22:11:54 +0300

    update profile

--- app/main/views.py
+++ app/main/views.py
@@ -11,14 +11,24 @@ def index():
     return render_template ('index.html')
 
 
-@main.route('/user/<uname>')
-def profile(uname):
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
-
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user)
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 
 @main.route('/pitch/new_pitches',methods=['POST','GET'])
 @login_required
@@ -39,4 +49,11 @@ def new_pitch():
     title = 'New pitch'
     return render_template('new_pitch.html',pitchform=pitch_form )
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
