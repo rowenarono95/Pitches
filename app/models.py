6f1f38387087d37b97dae2b9f98172847f98957b
rowenarono95 <rowenarono@gmail.com>
2020-10-27 17:41:27 +0300

    add bootstrap cdn

--- app/models.py
+++ app/models.py
@@ -14,8 +14,7 @@ class User(UserMixin,db.Model):
     password_secure = db.Column(db.String(255))
     pitches = db.relationship('Pitches',backref = 'user',lazy="dynamic")
     comments = db.relationship('Comment',backref =  'user',lazy = "dynamic")
-    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
-    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+   
 
 
     def __repr__(self):
@@ -43,8 +42,6 @@ class Pitches(db.Model):
     pitch_category = db.Column(db.String)
     pitch_comment = db.Column(db.String)
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
-    downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
     comments = db.relationship('Comment',backref =  'pitch',lazy = "dynamic")
 
     def save_pitch(self):
@@ -80,42 +77,4 @@ class Comment(db.Model):
         comments = Comment.query.filter_by(pitch_id=pitch).all()
         return comments  
 
-class Upvote(db.Model):
-    __tablename__ = 'upvotes'
-
-    id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-    
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_upvotes(cls,id):
-        upvote = Upvote.query.filter_by(pitch_id=id).all()
-        return upvote
-
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.pitch_id}'
-class Downvote(db.Model):
-    __tablename__ = 'downvotes'
-
-    id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-    
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-    @classmethod
-    def get_downvotes(cls,id):
-        downvote = Downvote.query.filter_by(pitch_id=id).all()
-        return downvote
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.pitch_id}'
      
