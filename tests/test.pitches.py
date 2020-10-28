028aa4358e5dcb47f7157975db5dd415809aa7e2
rowenarono95 <68608262+rowenarono95@users.noreply.github.com>
2020-10-28 15:08:23 +0300

    Update test.pitches.py

--- tests/test.pitches.py
+++ tests/test.pitches.py
@@ -1,27 +1,19 @@
-from ..models import Comment,User,Pitch
-from app import db
+
+        
+        
 import unittest
+from app.models import Pitches
 
-class PitchModelTest(unittest.TestCase):
+class PitchesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Pitches
+    '''
+
     def setUp(self):
-        self.user_rono = User(username = 'rono',password = 'banana', email = 'rowenarono@gmail.com')
-        self.new_pitch = Pitch(id=1,pitch_comment='This is a test pitch',category="interview",user = self.user_rono,likes=0,dislikes=0)
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_pitches = Pitches()
 
-    def tearDown(self):
-        User.query.delete()
-        Pitch.query.delete()
-      
-
-    def test_check_instance_variables(self):
-        self.assertEquals(self.new_pitch.pitch_comment,'This is a test pitch')
-        self.assertEquals(self.new_pitch.category,"interview")
-        self.assertEquals(self.new_pitch.user,self.user_rono)
-
-    def test_save_pitch(self):
-        self.new_pitch.save_pitch()
-        self.assertTrue(len(Pitch.query.all())>0)
-
-    def test_get_pitch_by_id(self):
-        self.new_pitch.save_pitch()
-        got_pitch = Pitch.get_pitch(1)
-        self.assertTrue(got_pitch is not None)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch,Pitches))
